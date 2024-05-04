2546c4e9622a492eef9d30a9691999710119329c
Santhosh Gandham <31979949+gandham-santhosh@users.noreply.github.com>
2024-05-04 21:34:53 +0530

    Adding sample ansible playbooks to learn and practice

---
+++ ansible-playbooks/test.sh
@@ -0,0 +1,14 @@
+NAME=$1
+
+function abc()
+{
+	echo "This is function abc $NAME"
+}
+
+function xyz()
+{
+	echo "This is function xyz $NAME"
+}
+echo "Hey! Bhargav this is sample bash $NAME"
+abc
+xyz
